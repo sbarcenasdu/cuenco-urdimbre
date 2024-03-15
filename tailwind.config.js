5fd7b538b28b3b5af2b9f9319140cc0bc9644131
Sebastian Bárcenas <sbarcenasd@unal.edu.co>
2024-03-15 08:05:19 -0500

    carga de archivos

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./src/**/*.html",
+    // "./src/**/*.astro",
+    "./src/layouts/Layout.astro",
+    "./src/components/SectionContainer.astro",
+    "./src/components/Main.astro",
+    "./src/components/Navbar.astro",
+    "./src/components/About.astro",
+    "./src/components/Meetings.astro",
+    "./src/components/Stats.astro",
+    "./src/components/Participants.astro",
+    "./src/components/Baseline.astro", 
+    "./src/components/Contact.astro",
+    "./src/components/HeroSection.astro",
+    "./src/components/HomeUrdimbre.astro",
+    "./src/components/StatsData.astro",
+    "./src/components/Events.astro",
+    "./src/components/Publications.astro",
+    "./src/components/Typologies.astro",
+
+  ],
+  theme: {
+    extend: {},
+  },
+};
